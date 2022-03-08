18c24b7c47b151d2b8694fbe7cf4299875d77147
Roxanna Coldiron <roxanna@coderedcorp.com>
2022-03-08 10:23:49 -0500

    analysis code and requirements.txt

--- eda-miami-dade-storms/miami_storms.py
+++ eda-miami-dade-storms/miami_storms.py
@@ -7,4 +7,41 @@ import statistics as st
 from rich_dataframe import prettify
 
 
-# Code coming soon!
+# Import csv into DataFrame
+miami = pd.read_csv(
+    "../datasets/miami-dade-storm-data.csv",
+    header=0,
+    sep=",",
+    low_memory=False,
+    parse_dates=["BEGIN_DATE"],
+)
+# Show the DataFrame
+print(miami)
+# Display the shape of the dataset
+print(f"The shape of the Miami-Dade County storm wind events dataset is {miami.shape}.")
+# Display descriptive statistics for numerical values in the dataset
+miami.describe()
+# Count totals of each wind event type in the dataset
+events = miami["EVENT_TYPE"]
+events.value_counts()
+# For this EDA, I am only interested in the dates and event types
+miami = miami[["BEGIN_DATE", "EVENT_TYPE"]]
+print(miami)
+# Plot the dates and events
+y = miami["EVENT_TYPE"]
+x = miami["BEGIN_DATE"]
+plt.plot_date(x, y)
+# Create sample sizes
+sample_twenty = miami.sample(20)
+sample_thirty = miami.sample(30)
+# Repeat the samples to make different samples and display in bar graphs
+(sample_twenty.groupby(["BEGIN_DATE", "EVENT_TYPE"]).size().unstack().plot.bar())
+(sample_thirty.groupby(["BEGIN_DATE", "EVENT_TYPE"]).size().unstack().plot.bar())
+# Count annual occurrences of storm wind events in Miami-Dade County
+events_annual = miami.groupby(pd.Grouper(key="BEGIN_DATE", axis=0, freq="1Y")).count()
+events_annual
+events_annual.describe()
+# Plot the annual frequencies for storm wind events
+ev_plot = events_annual.plot()
+ev_plot.set_xlabel("Year")
+ev_plot.set_ylabel = "Wind Events Frequency"
